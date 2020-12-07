7eb1cb27a5673ab92a076cfbbf6b03bdc8b2544d
Dany <danyramirezg@gmail.com>
2020-12-07 16:34:56 -0500

    Add lockboxes exercise

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+
+    numbers = []
+    unlock = []
+    len_boxes = len(boxes)
+
+    for box in boxes:
+        unlock.append(0)
+    unlock[0] = 1
+    # print(unlock)
+
+    for j in boxes[0]:
+        numbers.append(j)
+        # print(j)
+
+    for number in numbers:
+        if number < len_boxes and unlock[number] == 0:
+            unlock[number] = 1
+
+        for j in boxes[number]:
+            if j not in numbers:
+                numbers.append(j)
+
+    if 0 in unlock:
+        return False
+    else:
+        return True
