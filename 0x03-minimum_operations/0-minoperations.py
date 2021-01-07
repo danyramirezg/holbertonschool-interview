06eef285b9d987e6b67fe264b8663e40bb0453e4
danyramirezg <danyramirezg@gmail.com>
2021-01-07 15:17:17 -0500

    minoperations.py

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+
+def minOperations(n):
+    """text editor can execute only two operations in this file: Copy All and Paste.
+
+    Args:
+        n (int): write a method that calculates the fewest number
+
+    Returns:
+        int:  number of operations needed to result in exactly
+    """
+
+    inp = n
+    numOpperations = 0
+    module = 2
+
+    if n <= 1:
+        return 0
+
+    while inp > 1:
+        if inp % module == 0:
+            inp = inp / module
+            numOpperations = numOpperations + module
+        else:
+            module += 1
+    return numOpperations
